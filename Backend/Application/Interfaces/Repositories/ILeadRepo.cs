c9c742538ede2e7adac4bc697ee5d93d300e26fc
Hocine-Bec <bechebil.houcin@gmail.com>
2025-10-27 21:09:30 +0100

    feat(leads): add repository for Lead entity
    - Introduces the ILeadRepo interface and its implementation LeadRepo for data access.
    - The repository encapsulates the logic for querying Lead entities from the database.
    - Overrides GetAllAsync and GetByIdAsync to eager-load related Customer and Address data, preventing N+1 query issues.
    - This follows the repository pattern to separate data access concerns from the application's business logic.

---
+++ Backend/Application/Interfaces/Repositories/ILeadRepo.cs
@@ -0,0 +1,9 @@
+using Application.Interfaces.Repositories.Base;
+using Domain.Entities;
+
+namespace Application.Interfaces.Repositories;
+
+public interface ILeadRepo : IGenericRepo<Lead>
+{
+    
+}
